bd17883c9d1784aa7a5fc83595156b1b2b41b171
Sarvesh P <sarveshjaiofficial@gmail.com>
2025-02-13 10:21:34 +0530

    latest

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -7,6 +7,7 @@ export const connectToDb = async() => {
             throw new Error("DATABASE_URL is not defined");
         }
         mongoose.connect(process.env.DATABASE_URL);
+        console.log("connect to db")
     } catch (error) {
         console.error('Database connection error:', error);
         throw error;
